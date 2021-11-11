24d0b77dac971c2c0e8e7406d3c6fe816beca459
Drigo <rodrigoaguayo94@gmail.com>
2021-11-10 20:02:55 -0800

    have updated styles in navbar

--- src/components/landingpage/nav.jsx
+++ src/components/landingpage/nav.jsx
@@ -4,12 +4,14 @@ const Navbar = () => {
       <figure>
         <img src='https://via.placeholder.com/150' alt='logo' />
       </figure>
-      <input type='text' placeholder='Search..' />
-      <section>
-        <a href='#'>Login</a>
+      <div className='searchBar'>
+        <i className="fa fa-search icon"></i>
+        <input className='search' type='text' placeholder='Search..' />
+      </div>
+      <section className='links'>
+        <a href='#'>Create Account</a>
         <a href='#'>Cart</a>
-        <a href='#'>FAQ</a>
-        <a href='#'>Contact us</a>
+        <a href='#'>Contact</a>
       </section>
     </nav>
   );
